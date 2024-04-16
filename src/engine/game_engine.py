9cfcc5d54ad22b9a54021d9e3afe036ce8688a36
Jorge Pardo <jpardo@codema.coop>
2024-04-16 14:54:00 -0500

    creacion del sistema de coliciones jugador

--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -6,6 +6,7 @@ import esper
 from src.create.prefab_creator import create_input_player, create_player_square, create_square,create_enemy_spawner
 from src.ecs.components.c_input_command import CInputCommand, CommandPhase
 from src.ecs.components.c_velocity import CVelocity
+from src.ecs.systems.s_collision_player_enemy import system_collision_player_enemy
 from src.ecs.systems.s_enemy_spawner import system_enemy_spawner
 from src.ecs.systems.s_input_player import system_input_player
 from src.ecs.systems.s_movement import system_movement
@@ -78,6 +79,9 @@ class GameEngine:
         system_enemy_spawner(self.ecs_world,self.enemies_cfg,self.delta_time)
         system_movement(self.ecs_world, self.delta_time)
         system_screen_bounce(self.ecs_world, self.screen)
+        system_collision_player_enemy(self.ecs_world,self._player_entity,self.level_01_cfg)
+
+        self.ecs_world._clear_dead_entities()
 
     def _draw(self):
         self.screen.fill(self.bg_color)
@@ -85,6 +89,7 @@ class GameEngine:
         pygame.display.flip()
 
     def _clean(self):
+        self.ecs_world.clear_database()
         pygame.quit()
 
     def _do_action(self,c_input:CInputCommand):
